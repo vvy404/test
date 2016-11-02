706046a278627a7689571f646270be5dda2919c8
wangye <wangye@didichuxing.com>
2016-11-02 17:38:52 +0800

    H5demo

---
+++ main.js
@@ -0,0 +1,88 @@
+var fs = require('fs');
+var child_process = require('child_process');
+var readline = require('readline');
+
+var date = new Date(),
+	year = date.getFullYear(),
+	month = date.getMonth()+1,
+	day = date.getDate();
+
+var setDate1 = function(){
+	var str1 = year+'-'+month+'-'+day;
+	return str1;
+}
+var setDate2 = function(){
+	var str2 = year+month+day;
+	return str2;
+}
+
+console.log(date);
+
+function copy(src, dst, filename) {
+	if(!fs.existsSync(dst)){
+		child_process.execSync('mkdir -p '+dst);
+    }
+    fs.writeFileSync(dst+'/'+filename, fs.readFileSync(src));
+}
+function readFile(fileName){
+	if(fs.existsSync(fileName)) return fs.readFileSync(fileName,"utf-8");
+};
+
+function rexpFile(fileName,oldMsg,newMsg){
+	var data = readFile(fileName);
+	var exc = new RegExp(oldMsg,"g");
+	if(exc.test(data)){
+		console.log("已发现目标，等待替换~~~");
+		data = data.replace(exc,newMsg);
+		fs.writeFileSync(fileName, data);	
+		console.log('目标文件替换完毕');
+	}
+}
+
+function main(argv) {
+	var rl = readline.createInterface({
+	  input: process.stdin,
+	  output: process.stdout
+	});
+	rl.question('请输入文件夹名称 ', (answer) => {
+	  // TODO: Log the answer in a database
+	  var filename = answer;
+	  if(answer ==''){
+	  	filename=setDate1();
+	  }
+	  copy('./src/js/index.js', './dist/js','index.js');
+	  copy('./src/index.html', './dist','index.html');
+
+	  rexpFile('./dist/js/index.js','<%filename--%>',filename);
+	  rexpFile('./dist/index.html','<%filename--%>',filename);
+
+	  console.log('文件名已经替换:', answer);
+
+	  rl.question('是否需要配置share信息（y/n)? ', (answer) => {
+	  // TODO: Log the answer in a database
+		  switch(answer){
+		  	case 'y'||'Y':{
+		  		console.log("yes");
+		  		rl.question('请输入分享标题：', (answer) => {
+					rexpFile('./dist/js/index.js','<%sharetitle--%>',answer);
+					rexpFile('./dist/index.html','<%sharetitle--%>',answer);
+					rl.question('请输入分享内容：', (answer) => {
+						rexpFile('./dist/js/index.js','<%sharecontent--%>',answer);
+						rexpFile('./dist/index.html','<%sharecontent--%>',answer);
+			  		});
+		  		});
+		  		
+		  		break;
+		  	}
+		  	case 'n'||'N':{
+		  		console.log('no');
+		  		break;
+		  	}
+		  }
+		  
+		  // rl.close();
+		});
+	});    
+}
+
+main(process.argv.slice(2));
---
+++ src/js/index.js
@@ -0,0 +1,143 @@
+var base = dd.base || {};
+
+//判断环境，如果是支付宝则在body标签上加上‘alipay’
+var UAEnv = {
+    ua: navigator.userAgent.toLowerCase(),
+    isAlipay: false,
+    testAli: function(){
+        if (/alipay/i.test(ua)) {
+           isAdrAlipay = true;
+           return isAlipay; 
+        }
+    }
+};
+var musicControl = {
+    pauseMusic: document.querySelector("#playmusic"),
+    initMusic: function(){
+        base.touch(pauseMusic, function(ev) {
+            if (pauseMusic.className === "stopmusic") {
+                pauseMusic.classList.remove("stopmusic");
+                pauseMusic.classList.add("startmusic");
+                document.querySelector("audio").pause();
+                pauseMusic.querySelector("img").setAttribute("src", "images/stopmusic.png");
+            } else {
+                pauseMusic.classList.remove("startmusic");
+                pauseMusic.classList.add("stopmusic");
+                document.querySelector("audio").play();
+                pauseMusic.querySelector("img").setAttribute("src", "images/startmusic.gif");
+            }
+        });
+    }
+};
+var shareInfo = {
+    actMain: window.actMain,
+    share_data: {
+        share_url: 'http://static.diditaxi.com.cn/site/pages/<%filename--%>/index.html',
+        share_icon_url: 'http://static.diditaxi.com.cn/site/pages/<%filename--%>/images/share.jpg',
+        share_img_url: 'http://static.diditaxi.com.cn/site/pages/<%filename--%>/images/share.jpg',
+        share_title: '<%sharetitle--%>',
+        share_content: '<%sharecontent--%>',
+        share_from: "滴滴出行"
+    },
+    initShareInfo: function(){
+        if (actMain.isApp()) {
+            share_data.share_url = protocol + '//static.xiaojukeji.com/site/pages/<%filename--%>/pages/index.html?ddfrom=native&v=' + Math.random();
+        };
+        if (location.href.indexOf('ddfrom=native') != -1) {
+            var index = location.href.indexOf('v=') + 2;
+            var rand = location.href.substr(index);
+            var omaga_event = 'taxi_activity_20161020_native_' + rand;
+            Omega.trackEvent(omaga_event, 'native_shared');
+        };
+        didi.initShare(share_data);
+        weixin.initShare(share_data, function() {
+            actMain.ajax({
+                url: protocol + '//static.xiaojukeji.com/site/pages/<%filename--%>/pages/index.html?ddfrom=SNS&v=' + Math.random(),
+                method: 'GET'
+            });
+            Omega.trackEvent('taxi_activity_20161020', 'weixin_shared');
+        });
+    }
+    
+}
+
+window.addEventListener('DOMContentLoaded', function(ev) {
+
+// 初始化mySwiper
+    var mySwiper = new Swiper('.swiper-container', {
+        direction: 'vertical',
+        onlyExternal: false,
+        speed: 500
+    });
+    var btn1 = document.querySelector('.btn1');
+    var btn2 = document.querySelector('.btn2');
+    var mc1 = document.querySelector('.mc1');
+    var mc2 = document.querySelector('.mc2');
+    var playmusic = document.querySelector('#playmusic');
+
+    btn1.addEventListener('click', function() {
+        mc1.style.display = "block";
+        playmusic.style.display = "none";
+    })
+    btn2.addEventListener('click', function() {
+        mc2.style.display = "block";
+        playmusic.style.display = "none";
+    })
+    mc1.addEventListener('click', function() {
+        mc1.style.display = "none";
+        playmusic.style.display = "block";
+    })
+    mc2.addEventListener('click', function() {
+        mc2.style.display = "none";
+        playmusic.style.display = "block";
+    });
+
+
+
+    // //音乐开关设计
+
+    // var pauseMusic = document.querySelector("#playmusic");
+
+    // base.touch(pauseMusic, function(ev) {
+    //     if (pauseMusic.className === "stopmusic") {
+    //         pauseMusic.classList.remove("stopmusic");
+    //         pauseMusic.classList.add("startmusic");
+    //         document.querySelector("audio").pause();
+    //         pauseMusic.querySelector("img").setAttribute("src", "images/stopmusic.png");
+    //     } else {
+    //         pauseMusic.classList.remove("startmusic");
+    //         pauseMusic.classList.add("stopmusic");
+    //         document.querySelector("audio").play();
+    //         pauseMusic.querySelector("img").setAttribute("src", "images/startmusic.gif");
+    //     }
+    // });
+
+    var actMain = window.actMain;
+    var share_data = {
+        share_url: 'http://static.diditaxi.com.cn/site/pages/<%filename--%>/index.html',
+        share_icon_url: 'http://static.diditaxi.com.cn/site/pages/<%filename--%>/images/share.jpg',
+        share_img_url: 'http://static.diditaxi.com.cn/site/pages/<%filename--%>/images/share.jpg',
+        share_title: '<%sharetitle--%>',
+        share_content: '<%sharecontent--%>',
+        share_from: "滴滴出行"
+    };
+    if (actMain.isApp()) {
+        share_data.share_url = protocol + '//static.xiaojukeji.com/site/pages/<%filename--%>/pages/index.html?ddfrom=native&v=' + Math.random();
+    };
+    if (location.href.indexOf('ddfrom=native') != -1) {
+        var index = location.href.indexOf('v=') + 2;
+        var rand = location.href.substr(index);
+        var omaga_event = 'taxi_activity_20161020_native_' + rand;
+        Omega.trackEvent(omaga_event, 'native_shared');
+    };
+    didi.initShare(share_data);
+    weixin.initShare(share_data, function() {
+        actMain.ajax({
+            url: protocol + '//static.xiaojukeji.com/site/pages/<%filename--%>/pages/index.html?ddfrom=SNS&v=' + Math.random(),
+            method: 'GET'
+        });
+        Omega.trackEvent('taxi_activity_20161020', 'weixin_shared');
+    });
+}, !1);
+
+    console.log(process.argv.slice(2));
